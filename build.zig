f9ca763804ce70bbd9360300787d5ecb58a05149
tiawl <pabtomas@protonmail.com>
2024-11-02 20:53:56 +0100

    [update] fix oniguruma hash

--- build.zig
+++ build.zig
@@ -107,7 +107,7 @@ pub fn build (builder: *std.Build) !void
      .oniguruma = .{
        .name = "tiawl/oniguruma.zig",
        .host = toolbox.Repository.Host.github,
-       .ref = toolbox.Repository.Reference.tag,
+       .ref = toolbox.Repository.Reference.commit,
      },
      .winpthreads = .{
        .name = "kassane/winpthreads-zigbuild",
