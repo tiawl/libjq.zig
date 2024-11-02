834cfb4493a5f6eb47bc7fde4937f4572dd153ad
tiawl <pabtomas@protonmail.com>
2024-11-02 19:06:17 +0100

    [update] try to fix ntdll

--- build.zig
+++ build.zig
@@ -147,6 +147,7 @@ pub fn build (builder: *std.Build) !void
     }
     lib.linkLibrary (pthreads);
     lib.linkSystemLibrary ("shlwapi");
+    lib.linkSystemLibrary ("ntdll32");
   }
 
   const oniguruma_dep = builder.dependency ("oniguruma", .{
