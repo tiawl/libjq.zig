128b54b6bd45eb7c8e64755cdac9b810838a3c34
tiawl <pabtomas@protonmail.com>
2025-03-22 19:05:13 +0100

    update to toolbox 1.12.0

--- build.zig
+++ build.zig
@@ -2,130 +2,115 @@ const std = @import("std");
 const toolbox = @import("toolbox");
 
 const Paths = struct {
-    // prefixed attributes
-    __tmp: []const u8 = undefined,
-    __tmp_src: []const u8 = undefined,
-    __jq: []const u8 = undefined,
-    __jq_src: []const u8 = undefined,
+    __tmp: []const u8,
+    __tmp_src: []const u8,
+    __jq: []const u8,
+    __jq_src: []const u8,
 
-    // mandatory getters
-    pub fn getTmp(self: @This()) []const u8 {
+    fn getTmp(self: @This()) []const u8 {
         return self.__tmp;
     }
-    pub fn getTmpSrc(self: @This()) []const u8 {
+
+    fn getTmpSrc(self: @This()) []const u8 {
         return self.__tmp_src;
     }
-    pub fn getJq(self: @This()) []const u8 {
+
+    fn getJq(self: @This()) []const u8 {
         return self.__jq;
     }
-    pub fn getJqSrc(self: @This()) []const u8 {
+
+    fn getJqSrc(self: @This()) []const u8 {
         return self.__jq_src;
     }
 
-    // mandatory init
-    pub fn init(builder: *std.Build) !@This() {
-        var self = @This(){
-            .__jq = try builder.build_root.join(builder.allocator, &.{
-                "jq",
+    fn init() !@This() {
+        const jq_path = try toolbox.instance().getBuilder().build_root.join(toolbox.instance().getBuilder().allocator, &.{
+            "jq",
+        });
+        const tmp_path = try toolbox.instance().getBuilder().build_root.join(toolbox.instance().getBuilder().allocator, &.{
+            "tmp",
+        });
+
+        return .{
+            .__jq = jq_path,
+            .__tmp = tmp_path,
+            .__jq_src = toolbox.instance().ptrBuilder().pathJoin(&.{
+                jq_path, "src",
             }),
-            .__tmp = try builder.build_root.join(builder.allocator, &.{
-                "tmp",
+            .__tmp_src = toolbox.instance().ptrBuilder().pathJoin(&.{
+                tmp_path, "src",
             }),
         };
-
-        self.__jq_src = try std.fs.path.join(builder.allocator, &.{
-            self.getJq(),
-            "src",
-        });
-        self.__tmp_src = try std.fs.path.join(builder.allocator, &.{
-            self.getTmp(),
-            "src",
-        });
-
-        return self;
     }
 };
 
-fn update(builder: *std.Build, path: *const Paths, dependencies: *const toolbox.Dependencies) !void {
-    std.fs.deleteTreeAbsolute(path.getJq()) catch |err|
-        {
-            switch (err) {
-                error.FileNotFound => {},
-                else => return err,
-            }
-        };
+fn update(path: *const Paths, dependencies: *const toolbox.Dependencies) !void {
+    std.fs.deleteTreeAbsolute(path.getJq()) catch |err| {
+        switch (err) {
+            error.FileNotFound => {},
+            else => return err,
+        }
+    };
 
-    try dependencies.clone(builder, "jq", path.getTmp());
-    try toolbox.run(builder, .{
+    try dependencies.clone("jq", path.getTmp());
+    try toolbox.instance().run(.{
         .argv = &[_][]const u8{
-            "git",
-            "submodule",
-            "update",
-            "--init",
+            "git", "submodule", "update", "--init",
         },
         .cwd = path.getTmp(),
     });
-    try toolbox.run(builder, .{
+    try toolbox.instance().run(.{
         .argv = &[_][]const u8{
-            "autoreconf",
-            "-i",
+            "autoreconf", "-i",
         },
         .cwd = path.getTmp(),
     });
-    try toolbox.run(builder, .{
+    try toolbox.instance().run(.{
         .argv = &[_][]const u8{
-            "./configure",
-            "--disable-docs",
-            "--disable-valgrind",
-            "--with-oniguruma=builtin",
+            "./configure", "--disable-docs", "--disable-valgrind", "--with-oniguruma=builtin",
         },
         .cwd = path.getTmp(),
     });
-    try toolbox.run(builder, .{
+    try toolbox.instance().run(.{
         .argv = &[_][]const u8{
-            "make",
-            "-j8",
+            "make", "-j8",
         },
         .cwd = path.getTmp(),
     });
 
-    try toolbox.make(path.getJq());
-    try toolbox.make(path.getJqSrc());
+    try toolbox.instance().make(path.getJq());
+    try toolbox.instance().make(path.getJqSrc());
 
     var src_dir = try std.fs.openDirAbsolute(path.getTmpSrc(), .{
         .iterate = true,
     });
     defer src_dir.close();
 
-    var walker = try src_dir.walk(builder.allocator);
+    var walker = try src_dir.walk(toolbox.instance().getBuilder().allocator);
     defer walker.deinit();
 
     while (try walker.next()) |*entry| {
-        const dest = try std.fs.path.join(builder.allocator, &.{
-            path.getJqSrc(),
-            entry.path,
+        const dest = toolbox.instance().ptrBuilder().pathJoin(&.{
+            path.getJqSrc(), entry.path,
         });
         switch (entry.kind) {
-            .file => try toolbox.copy(try std.fs.path.join(builder.allocator, &.{
-                path.getTmpSrc(),
-                entry.path,
+            .file => try toolbox.instance().copy(toolbox.instance().ptrBuilder().pathJoin(&.{
+                path.getTmpSrc(), entry.path,
             }), dest),
-            .directory => try toolbox.make(dest),
+            .directory => try toolbox.instance().make(dest),
             else => return error.UnexpectedEntryKind,
         }
     }
 
     try std.fs.deleteTreeAbsolute(path.getTmp());
-    try std.fs.deleteTreeAbsolute(try std.fs.path.join(builder.allocator, &.{
-        path.getJqSrc(),
-        "inject_errors.c",
+    try std.fs.deleteTreeAbsolute(toolbox.instance().ptrBuilder().pathJoin(&.{
+        path.getJqSrc(), "inject_errors.c",
     }));
-    try std.fs.deleteTreeAbsolute(try std.fs.path.join(builder.allocator, &.{
-        path.getJqSrc(),
-        "main.c",
+    try std.fs.deleteTreeAbsolute(toolbox.instance().ptrBuilder().pathJoin(&.{
+        path.getJqSrc(), "main.c",
     }));
 
-    try toolbox.clean(builder, &.{
+    try toolbox.instance().clean(&.{
         "jq",
     }, &.{
         ".inc",
@@ -136,9 +121,9 @@ pub fn build(builder: *std.Build) !void {
     const target = builder.standardTargetOptions(.{});
     const optimize = builder.standardOptimizeOption(.{});
 
-    const path = try Paths.init(builder);
-
-    const dependencies = try toolbox.Dependencies.init(builder, .libjq_zig, "0x4fefb366172605fb", &.{
+    toolbox.init(builder, optimize);
+    defer toolbox.deinit();
+    const dependencies = try toolbox.Dependencies.init(.libjq_zig, "0x4fefb366172605fb", &.{
         "jq",
     }, .{
         .toolbox = .{
@@ -159,23 +144,26 @@ pub fn build(builder: *std.Build) !void {
         },
     });
 
-    if (builder.option(bool, "update", "Update binding") orelse false)
-        try update(builder, &path, &dependencies);
+    const path = try Paths.init();
 
-    const lib = builder.addStaticLibrary(.{
+    if (toolbox.instance().ptrBuilder().option(bool, "update", "Update binding") orelse false) {
+        try update(&path, &dependencies);
+    }
+
+    const lib = toolbox.instance().ptrBuilder().addStaticLibrary(.{
         .name = "jq",
-        .root_source_file = builder.addWriteFiles().add("empty.c", ""),
+        .root_source_file = toolbox.instance().ptrBuilder().addWriteFiles().add("empty.c", ""),
         .target = target,
         .optimize = optimize,
     });
 
-    toolbox.addInclude(lib, "jq");
+    toolbox.instance().addInclude(lib, "jq");
 
     if (lib.rootModuleTarget().isMinGW()) {
         lib.linkSystemLibrary("shlwapi");
     }
 
-    const oniguruma_dep = builder.dependency("oniguruma_zig", .{
+    const oniguruma_dep = toolbox.instance().ptrBuilder().dependency("oniguruma_zig", .{
         .target = target,
         .optimize = optimize,
     });
@@ -185,23 +173,24 @@ pub fn build(builder: *std.Build) !void {
 
     lib.linkLibC();
 
-    toolbox.addHeader(lib, path.getJqSrc(), ".", &.{
-        ".h",
-        ".inc",
+    toolbox.instance().addHeader(lib, path.getJqSrc(), ".", &.{
+        ".h", ".inc",
     });
 
-    var jq_src_dir =
-        try std.fs.openDirAbsolute(path.getJqSrc(), .{
-            .iterate = true,
-        });
+    var jq_src_dir = try std.fs.openDirAbsolute(path.getJqSrc(), .{
+        .iterate = true,
+    });
     defer jq_src_dir.close();
 
-    const flags = [_][]const u8{ "-DIEEE_8087=1", "-D_GNU_SOURCE=1", "-DHAVE_LIBONIG=1" };
+    const flags = [_][]const u8{
+        "-DIEEE_8087=1", "-D_GNU_SOURCE=1", "-DHAVE_LIBONIG=1",
+    };
     var it = jq_src_dir.iterate();
     while (try it.next()) |*entry| {
-        if (toolbox.isCSource(entry.name) and entry.kind == .file)
-            try toolbox.addSource(lib, path.getJqSrc(), entry.name, &flags);
+        if (toolbox.isCSource(entry.name) and entry.kind == .file) {
+            try toolbox.instance().addSource(lib, path.getJqSrc(), entry.name, &flags);
+        }
     }
 
-    builder.installArtifact(lib);
+    toolbox.instance().ptrBuilder().installArtifact(lib);
 }
